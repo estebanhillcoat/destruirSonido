0e4e2dc80d7fd1d73db804e0dedcefc7244ad83b
esteban Hillcoat <43394400+estebanhillcoat@users.noreply.github.com>
2021-11-26 14:51:43 -0300

    Create sonidoDestroy.cs
    Podes destruir un objeto haciendo que emita un sonido. 
    
    PlayClipAtPoint reproduce un clip en un lugar especificado en el 2do parametro y luego lo elimina.

---
+++ sonidoDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectar : MonoBehaviour
+{
+
+    public AudioSource quienEmite;
+    public AudioClip elAudioDeMario;
+    public float volumen = 1;
+
+    private void OnTriggerEnter(Collider other) {
+        
+
+        AudioSource.PlayClipAtPoint(elAudioDeMario,gameObject.transform.position);
+        Destroy(gameObject);
+
+}
