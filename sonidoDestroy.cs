0681d2b9c29cb0f4c2101155227b0ecfabdae68b
esteban Hillcoat <43394400+estebanhillcoat@users.noreply.github.com>
2021-11-26 14:53:54 -0300

    Update sonidoDestroy.cs

--- sonidoDestroy.cs
+++ sonidoDestroy.cs
@@ -9,10 +9,21 @@ public class detectar : MonoBehaviour
     public AudioClip elAudioDeMario;
     public float volumen = 1;
 
+
+
     private void OnTriggerEnter(Collider other) {
+        
+// si antes se escucho el sonido no hacer nada
+
+
         
 
         AudioSource.PlayClipAtPoint(elAudioDeMario,gameObject.transform.position);
-        Destroy(gameObject);
+        //Destroy(gameObject);
 
-}
+    }
+    
+    }
+
+
+
